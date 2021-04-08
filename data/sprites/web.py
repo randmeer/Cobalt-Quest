0518f9db99edf2837d284f33fe64e61067a05b8e
Randmeer <das.randmeer@gmail.com>
2021-04-08 10:26:15 +0200

    victims break webs after certain time

--- data/sprites/web.py
+++ data/sprites/web.py
@@ -1,17 +1,12 @@
 import pygame, random
 from data import globals
 
-shrinkx = 20
-shrinky = 20
-
 
 class Web(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        # self.image = pygame.Surface((100, 100))
         self.image = pygame.transform.scale(pygame.image.load("data/textures/web.png"), (50, 50))
-        # self.image.fill(WHITE)
         self.rect = self.image.get_rect()
 
     def summon(self):
