855086fa84b5e3cfba229b6e935a88e34fb1b87e
Randmeer <das.randmeer@gmail.com>
2021-06-22 23:42:42 +0200

    reworked stats gui & merged selection gui

--- sprites/gui.py
+++ sprites/gui.py
@@ -1,43 +1,44 @@
 import pygame
-
+import globs
 from sprites import label
-from utils import relToAbsDual, relToAbs
+from utils import relToAbsDual, relToAbs, renderText
 
 selection_texture = pygame.image.load("textures/selection.png")
 dagger_texture = pygame.image.load("textures/dagger.png")
 shuriken_texture = pygame.image.load("textures/shuriken.png")
 bow_texture = pygame.image.load("textures/bow.png")
 item_texture = pygame.image.load("textures/cross.png")
-
-# GUI class is unfinished, working on it. ~Rande
+heart_texture = pygame.image.load("textures/heart.png")
+ichkeksi_texture = pygame.image.load("textures/ichkeksi.png")
+tick_texture = pygame.image.load("textures/tick.png")
+cross_texture = pygame.image.load("textures/cross.png")
+broken_heart_texture = pygame.image.load("textures/broken_heart.png")
 
 class GUI(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        # weapon and block hotbars
+        self.weapons = [["dagger", -1], ["katana", -1], ["shuriken", 25], ["bow", 10], ["unknown", -1], ["unknown", -1]]
+        self.blocks = [["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10], ["block", 10]]
         self.centers = [(0.075, 0.925), (0.200, 0.925), (0.325, 0.925), (0.450, 0.925), (0.575, 0.925), (0.700, 0.925)]
-        self.centers_small = [(0.050, 0.825), (0.125, 0.825), (0.200, 0.825), (0.275, 0.825)]
-        self.rects = [pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0),
-                      pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0)]
-        self.rects_small = [pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0),
-                            pygame.Rect(0, 0, 0, 0), pygame.Rect(0, 0, 0, 0)]
-        self.items = [["dagger", -1], ["katana", -1], ["shuriken", 25], ["bow", 10], ["unknown", -1], ["unknown", -1]]
-        self.itemlabels = []
-        self.weapon, self.block = 0, 0
-
+        self.centers_small = [(0.050, 0.825), (0.125, 0.825), (0.200, 0.825), (0.275, 0.825), (0.350, 0.825), (0.425, 0.825), (0.500, 0.825), (0.575, 0.825), (0.650, 0.825), (0.725, 0.825)]
+        self.rects, self.rects_small, self.itemlabels = [], [], []
+        for i in range(len(self.weapons)):
+            self.rects.append(pygame.Rect(0, 0, 0, 0))
+        for i in range(len(self.blocks)):
+            self.rects_small.append(pygame.Rect(0, 0, 0, 0))
+        self.weapon = self.block = 0
         self.resize()
         self.update()
 
     def resize(self):
-        # levelname and progress
         self.titlangle = pygame.Surface(relToAbsDual(0.725, 0.11))
         self.titlangle.fill((0, 0, 0))
         self.titlangle.set_alpha(75)
-
-        self.floorlabel = label.Label(text="3rd Floor:", anchor="topleft", reltextsize=0.05,
-                                      relanchorpointposition=(0.035, 0.035), textcolor=(255, 255, 255))
-        self.titlelabel = label.Label(text="Menga's Hideout", anchor="topleft", reltextsize=0.05,
-                                      relanchorpointposition=(0.035, 0.085), textcolor=(255, 255, 255))
+        self.statlangle = pygame.Surface(relToAbsDual(0.725, 0.11))
+        self.statlangle.fill((0, 0, 0))
+        self.statlangle.set_alpha(75)
+        self.floorlabel = label.Label(text="3rd Floor:", anchor="topleft", reltextsize=0.05, relanchorpointposition=(0.035, 0.035), textcolor=(255, 255, 255))
+        self.titlelabel = label.Label(text="Menga's Hideout", anchor="topleft", reltextsize=0.05, relanchorpointposition=(0.035, 0.085), textcolor=(255, 255, 255))
         self.itemtextures = [pygame.transform.scale(dagger_texture, relToAbsDual(0.08, 0.08)),
                              pygame.transform.scale(item_texture, relToAbsDual(0.08, 0.08)),
                              pygame.transform.scale(shuriken_texture, relToAbsDual(0.08, 0.08)),
@@ -51,43 +52,49 @@ class GUI(pygame.sprite.Sprite):
         self.overlangle.set_alpha(75)
         self.overlangle_small = pygame.transform.scale(self.overlangle, relToAbsDual(0.05, 0.05))
 
-        for i in range(6):
+        for i in range(len(self.rects)):
             self.rects[i].size = relToAbsDual(0.1, 0.1)
             self.rects[i].center = relToAbsDual(self.centers[i][0], self.centers[i][1])
-        for i in range(4):
+        for i in range(len(self.rects_small)):
             self.rects_small[i].size = relToAbsDual(0.05, 0.05)
             self.rects_small[i].center = relToAbsDual(self.centers_small[i][0], self.centers_small[i][1])
 
     def update(self):
         self.itemlabels = []
-        for i in range(len(self.items)):
-            if self.items[i][1] != -1:
-                self.itemlabels.append(label.Label(text=str(self.items[i][1]), anchor="bottomleft", reltextsize=0.04,
-                                                   relanchorpointposition=(self.centers[i][0] - 0.045,
-                                                                           self.centers[i][1] + 0.045),
-                                                   textcolor=(255, 255, 255)))
-        if self.weapon >= 6:
+        for i in range(len(self.weapons)):
+            if self.weapons[i][1] != -1:
+                self.itemlabels.append(label.Label(text=str(self.weapons[i][1]), anchor="bottomleft", reltextsize=0.04, relanchorpointposition=(self.centers[i][0] - 0.045, self.centers[i][1] + 0.045), textcolor=(255, 255, 255)))
+        if self.weapon >= len(self.rects):
             self.weapon = 0
         elif self.weapon < 0:
-            self.weapon = 4
-        if self.block >= 4:
+            self.weapon = len(self.rect)-1
+        if self.block >= len(self.rects_small):
             self.block = 0
         elif self.block < 0:
-            self.block = 3
+            self.block = len(self.rects_small)-1
 
     def draw(self, window):
         for i in range(len(self.rects)):
             window.blit(self.overlangle, self.rects[i])
-            window.blit(self.itemtextures[i], (self.rects[i].x + relToAbs(0.01), self.rects[i].y + relToAbs(0.01)))
+            window.blit(self.itemtextures[i], (self.rects[i].x+relToAbs(0.01), self.rects[i].y+relToAbs(0.01)))
         for i in range(len(self.rects_small)):
             window.blit(self.overlangle_small, self.rects_small[i])
-        window.blit(self.selectangle,
-                    (self.rects[self.weapon].x - relToAbs(0.01), self.rects[self.weapon].y - relToAbs(0.01)))
-        window.blit(self.selectangle_small, (
-        self.rects_small[self.block].x - relToAbs(0.005), self.rects_small[self.block].y - relToAbs(0.005)))
+        window.blit(self.selectangle, (self.rects[self.weapon].x - relToAbs(0.01), self.rects[self.weapon].y - relToAbs(0.01)))
+        window.blit(self.selectangle_small, (self.rects_small[self.block].x - relToAbs(0.005), self.rects_small[self.block].y - relToAbs(0.005)))
         for i in self.itemlabels:
             i.update()
             i.draw(window=window)
         window.blit(self.titlangle, relToAbsDual(0.025, 0.025))
+        window.blit(self.statlangle, relToAbsDual(0.025, 0.150))
         self.floorlabel.draw(window=window)
         self.titlelabel.draw(window=window)
+        renderText(window, str(int(globs.playerhealthpoints)), relToAbsDual(0.085, 0.160), globs.WHITE, relToAbs(0.048))
+        renderText(window, str(globs.victimspawns - globs.victimsmissed - globs.victimskilled + 1), relToAbsDual(0.254, 0.160), globs.WHITE, relToAbs(0.048))
+        renderText(window, str(globs.victimskilled), relToAbsDual(0.43, 0.160), globs.WHITE, relToAbs(0.048))
+        renderText(window, str(globs.victimsmissed), relToAbsDual(0.61, 0.160), globs.WHITE, relToAbs(0.048))
+        renderText(window, str(globs.damagesum), relToAbsDual(0.085, 0.215), globs.WHITE, relToAbs(0.048))
+        window.blit(pygame.transform.scale(heart_texture, relToAbsDual(0.036, 0.036)), relToAbsDual(0.035, 0.160))
+        window.blit(pygame.transform.scale(ichkeksi_texture, relToAbsDual(0.04, 0.04)), relToAbsDual(0.2, 0.160))
+        window.blit(pygame.transform.scale(tick_texture, relToAbsDual(0.036, 0.036)), relToAbsDual(0.38, 0.160))
+        window.blit(pygame.transform.scale(cross_texture, relToAbsDual(0.036, 0.036)), relToAbsDual(0.56, 0.160))
+        window.blit(pygame.transform.scale(broken_heart_texture, relToAbsDual(0.036, 0.036)), relToAbsDual(0.035, 0.215))
